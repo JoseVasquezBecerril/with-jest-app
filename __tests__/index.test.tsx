7ed86e398e5f7de6f7660b5018a0cbfbd9c52b41
Jose Carlos Vasquez Cruz <josecarlosvasquez@gmail.com>
2024-05-04 09:19:12 -0500

    Ajuste tests

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -9,7 +9,7 @@ describe("Home", () => {
     render(<Home />);
 
     const heading = screen.getByRole("heading", {
-      name: /welcome Home to next\.js!/i,
+      name: /welcome to next\.js!/i,
     });
 
     expect(heading).toBeInTheDocument();
